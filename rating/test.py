06db8130e37fc67b2b70cbe0c0fc434e2590608c
Mars <houjianchao1204@gmail.com>
2018-04-12 18:15:10 +0800

    home page add attr of no

--- rating/test.py
+++ rating/test.py
@@ -1,5 +1,28 @@
+# import data
+# # 414478124 1084660392
+# jsonb = data.get_data(414478124)
+# print(jsonb)
 
-import data
 
-jsonb = data.get_data(1084660392)
-print(jsonb)
+# def fab(max):
+#     n, a, b = 0, 0, 1
+#     while n < max:
+#         yield b
+#         a, b = b, a + b
+#         n += 1
+#
+#
+# def fab2(max):
+#     yield range(1, max)
+#
+#
+# f = fab(5)
+#
+# for s in f:
+#     print(s)
+# print(f.next())
+# print(f.next())
+# print(f.next())
+# print(f.next())
+# print(f.next())
+# print(f.next())
